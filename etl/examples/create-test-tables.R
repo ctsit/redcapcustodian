2a7d0b06144543148a5bb9c147fe6a9b8397369a
Kyle Chesney <kyle.2493@gmail.com>
2022-02-10 11:24:02 -0500

    fix create-test-tables, include packages and use map2

--- etl/examples/create-test-tables.R
+++ etl/examples/create-test-tables.R
@@ -1,6 +1,20 @@
+library(tidyverse)
+library(redcapcustodian)
+
+# create SQLite table in memory
+conn <- DBI::dbConnect(RSQLite::SQLite(), dbname = ":memory:")
+
 tables <- c(
   "redcap_projects",
   "redcap_user_information"
 )
 
-tables %>% map(redcap_custodian::create_test_table())
+map2(
+  .x = c(conn),
+  .y = tables,
+  .f = create_test_table
+)
+
+# check contents of each table
+tbl(conn, "redcap_projects")
+tbl(conn, "redcap_user_information")
