c55cc98f428dd8c1a2f6913037a7bb2fffa1476c
Philip Chase <pbc@ufl.edu>
2022-05-16 13:35:01 -0400

    Add dataset_diff

---
+++ R/data.R
@@ -0,0 +1,25 @@
+#' @title dataset_diff_test_bar_bang
+#' @description A list of test inputs and outputs for dataset_diff
+#' @format A list of 5 variables:
+#' \describe{
+#'   \item{\code{source}}{tibble of the source data}
+#'   \item{\code{source_pk}}{character primary key of the source data}
+#'   \item{\code{target}}{tibble of the target data}
+#'   \item{\code{target_pk}}{character primary key of the target data}
+#'   \item{\code{result}}{list of dataset_diff output}
+#'}
+#' @details DETAILS
+"dataset_diff_test_bar_bang"
+
+#' @title dataset_diff_test_user_data
+#' @description A list of test inputs and outputs for dataset_diff
+#' @format A list of 5 variables:
+#' \describe{
+#'   \item{\code{source}}{tibble of the source data}
+#'   \item{\code{source_pk}}{character primary key of the source data}
+#'   \item{\code{target}}{tibble of the target data}
+#'   \item{\code{target_pk}}{character primary key of the target data}
+#'   \item{\code{result}}{list of dataset_diff output}
+#'}
+#' @details DETAILS
+"dataset_diff_test_user_data"
---
+++ data-raw/dataset_diff_data.R
@@ -0,0 +1,109 @@
+## code to prepare dataset for testing dataset_diff goes here
+library(dplyr)
+
+target <- dplyr::tribble(
+  ~id, ~pk, ~bar, ~bang,
+  1,1,1,1,
+  2,2,2,1,
+  3,3,3,1,
+  4,4,4,1,
+  6,6,6,1
+)
+
+source <- dplyr::tribble(
+  ~pk, ~bar,
+  1,1,
+  2,2,
+  3,1,
+  4,1,
+  5,5
+)
+
+result <-
+  list(
+    update_records = tribble(
+      ~id, ~pk, ~bar,
+      3, 3, 1,
+      4, 4, 1
+    ),
+    insert_records = tribble(
+      ~pk,   ~bar,
+      5,     5
+    ),
+    delete_records = tribble(
+      ~id, ~pk, ~bar, ~bang,
+      6, 6, 6, 1
+    )
+  )
+
+# result <- dataset_diff(source = source,
+#              source_pk = "pk",
+#              target = target,
+#              target_pk = "id"
+# )
+
+dataset_diff_test_bar_bang <- list(
+  source = source,
+  source_pk = "pk",
+  target = target,
+  target_pk = "id",
+  result = result
+)
+
+usethis::use_data(dataset_diff_test_bar_bang, overwrite = TRUE)
+
+# user_data is a test that should return two  records for email address update
+target <- dplyr::tribble(
+  ~ui_id, ~username, ~user_email,
+  1,"alice","alice@example.org",
+  2,"bob","robert@example.org",
+  3,"carol","carol@example.org",
+  4,"dan","daniel@example.org"
+)
+
+source <- dplyr::tribble(
+  ~username, ~user_email,
+  "alice","alice@example.org",
+  "bob","bob@example.org",
+  "carol","carol@example.org",
+  "dan","dan@example.org"
+)
+
+# result <-
+#   dataset_diff(source = source,
+#                        source_pk = "username",
+#                        target = target,
+#                        target_pk = "ui_id"
+# )
+
+result <-
+  list(
+    update_records = tribble(
+      ~ui_id, ~username, ~user_email,
+      2,"bob","bob@example.org",
+      4,"dan","dan@example.org"
+    ),
+    insert_records = tribble(
+      ~username, ~user_email
+    ) %>%
+      mutate(
+        dplyr::across(c("username", "user_email"), as.character)
+      ),
+    delete_records = tribble(
+      ~ui_id, ~username, ~user_email
+    ) %>%
+      mutate(
+        dplyr::across(c("ui_id"), as.numeric),
+        dplyr::across(c("username", "user_email"), as.character)
+      )
+  )
+
+dataset_diff_test_user_data <- list(
+  source = source,
+  source_pk = "username",
+  target = target,
+  target_pk = "ui_id",
+  result = result
+)
+
+usethis::use_data(dataset_diff_test_user_data, overwrite = TRUE)
