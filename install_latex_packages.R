cbac6857882e3a50e365edef1ffc09d39b05dedb
Laurence James-Woodley <mr.lawjames@gmail.com>
2022-12-14 21:24:10 -0500

    install latex packages in dockerfile

--- install_latex_packages.R
+++
@@ -1,2 +0,0 @@
-packages_to_install <- read.csv("https://raw.githubusercontent.com/ljwoodley/redcapcustodian/add_latex_packages/latex_packages.csv")
-tinytex::tlmgr_install(packages_to_install$package)
