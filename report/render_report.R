9974b583250bc95fbd0de91a8a49ff4dbdff5c24
Laurence James-Woodley <mr.lawjames@gmail.com>
2023-01-07 11:42:00 -0500

    add render_report to /report

---
+++ report/render_report.R
@@ -0,0 +1,42 @@
+library(tidyverse)
+library(dotenv)
+library(REDCapR)
+library(lubridate)
+library(rmarkdown)
+library(sendmailR)
+library(redcapcustodian)
+
+init_etl("render_report")
+
+if (!interactive()) {
+  args <- commandArgs(trailingOnly = T)
+  script_name <- word(args, 2, sep = "=")
+} else {
+  script_name <- "sample_report.Rmd"
+}
+
+report_name <- word(script_name, 1, sep = "\\.")
+
+script_run_time <- set_script_run_time()
+
+output_file = paste0(report_name,
+                     "_",
+                     format(script_run_time, "%Y%m%d%H%M%S"))
+
+full_path_to_output_file <- render(
+  paste0("report/", script_name),
+  output_file = output_file
+)
+
+output_file_extension <- word(full_path_to_output_file, 2 , sep = "\\.")
+attachment_object <- mime_part(full_path_to_output_file, paste0(output_file, ".", output_file_extension))
+
+email_subject <- paste(report_name, "|", script_run_time)
+body <- "Please see the attached report."
+
+email_body <- list(body, attachment_object)
+
+# send the email with the attached output file
+send_email(email_body, email_subject)
+
+# log_job_success(jsonlite::toJSON(script_name))
