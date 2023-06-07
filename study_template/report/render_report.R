84af9836831d9355f062223bb76d8005402bc996
Philip Chase <pbc@ufl.edu>
2023-06-07 16:06:23 -0400

    Update render_report.R to support quarto files
    Add quarto support.
    Use argparse in render_report.R to provide better error handling.
    Add argparse package to Dockerfile.
    Update docs to reflect the new feature.
    Update example files to reflect a syntax change.

--- study_template/report/render_report.R
+++ study_template/report/render_report.R
@@ -5,31 +5,49 @@ library(lubridate)
 library(rmarkdown)
 library(sendmailR)
 library(redcapcustodian)
+library(argparse)
 
 init_etl("render_report")
 
+parser <- ArgumentParser()
+parser$add_argument("script_name", nargs=1, help="Script to be run")
 if (!interactive()) {
-  args <- commandArgs(trailingOnly = T)
-  script_name <- word(args, 2, sep = "=")
+  args <- parser$parse_args()
+  script_name <- args$script_name
+  if(!fs::file_exists(script_name)) {
+    stop(sprintf("Specified file, %s, does not exist", script_name))
+  }
 } else {
-  script_name <- "sample_report.Rmd"
+  script_name <- "dummy.qmd"
+  stop(sprintf("Specified file, %s, does not exist", script_name))
 }
 
 report_name <- word(script_name, 1, sep = "\\.")
+report_type <- word(script_name, 2, sep = "\\.")
 
 script_run_time <- set_script_run_time()
+output_file <-
+  paste0(str_replace(report_name, ".*/", ""),
+         "_",
+         format(script_run_time, "%Y%m%d%H%M%S"),
+         if_else(report_type == "qmd", ".pdf", "")
+         )
 
-output_file = paste0(report_name,
-                     "_",
-                     format(script_run_time, "%Y%m%d%H%M%S"))
+if (report_type == "qmd") {
+  quarto::quarto_render(
+    script_name,
+    output_file = output_file,
+    output_format = "pdf"
+  )
+} else {
+  render(
+    script_name,
+    output_file = output_file
+  )
+}
 
-full_path_to_output_file <- render(
-  paste0("report/", script_name),
-  output_file = output_file
-)
-
-output_file_extension <- word(full_path_to_output_file, 2 , sep = "\\.")
-attachment_object <- mime_part(full_path_to_output_file, paste0(output_file, ".", output_file_extension))
+output_file_extension <- word(output_file, 2 , sep = "\\.")
+attachment_object <- mime_part(output_file, output_file)
 
 email_subject <- paste(report_name, "|", script_run_time)
 body <- "Please see the attached report."
