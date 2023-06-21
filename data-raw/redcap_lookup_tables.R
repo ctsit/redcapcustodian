6697f4356f8caae4ea2da6417842d1d723080471
Philip Chase <pbc@ufl.edu>
2023-06-21 16:59:06 -0400

    Add project_purpose_other_research_labels.rda

--- data-raw/redcap_lookup_tables.R
+++ data-raw/redcap_lookup_tables.R
@@ -9,6 +9,19 @@ project_purpose_labels <- dplyr::tribble(
 # write the data
 usethis::use_data(project_purpose_labels, overwrite = T)
 
+project_purpose_other_research_labels <- dplyr::tribble(
+  ~id, ~project_purpose_other_research,
+  0, "Basic or bench research",
+  1, "Clinical research study or trial",
+  2, "Translational research 1 (applying discoveries to the development of trials and studies in humans)",
+  3, "Translational research 2 (enhancing adoption of research findings and best practices into the community)",
+  4, "Behavioral or psychosocial research study",
+  5, "Epidemiology",
+  6, "Repository (developing a data or specimen repository for future use by investigators)",
+  7, "Other"
+)
+usethis::use_data(project_purpose_other_research_labels, overwrite = T)
+
 project_status_labels <- dplyr::tribble(
   ~id, ~project_status,
   0, "Development",
