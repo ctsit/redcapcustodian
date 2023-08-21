5a284f7824d395ef7b7bcf99f300f9c79d3400ca
Laurence James-Woodley <mr.lawjames@gmail.com>
2023-08-21 19:42:25 -0400

    add record_id column and minimal necessay redcap validation types

--- R/dataframe_to_redcap_dictionary.R
+++ R/dataframe_to_redcap_dictionary.R
@@ -1,8 +1,7 @@
-#' dataframe_to_redcap_dictionary
-#'
-#' Create a REDCap data dictionary for a dataframe
+#' Create a REDCap data dictionary from a dataframe
 #'
 #' @param df the dataframe to generate the data dictionary for
+#' @param record_id_col a column in the dataframe that uniquely  identifies each record
 #' @param form_name  the form name to display in REDCap
 #' @param write_to_csv If TRUE will write the data dictionary to a csv.
 #' @param filename A string specifying the filename for the CSV.
@@ -14,55 +13,80 @@
 #' \dontrun{
 #'
 #' df <- data.frame(
-#'   numeric_col = c(1, 2, 3),
-#'   integer_col = as.integer(5.9),
+#'   pk_col = c("a1", "a2", "a3"),
+#'   integer_col = c(1, 2, 3),
+#'   numeric_col = 5.9,
 #'   character_col = c("a", "b", "c"),
 #'   date_col = as.Date("2011-03-07"),
-#'   date_time_col = as.POSIXct(strptime("2011-03-27 01:30:00", "%Y-%m-%d %H:%M:%S"))
+#'   date_time_col = as.POSIXct(strptime("2011-03-27 01:30:00", "%Y-%m-%d %H:%M:%S")),
+#'   email_col = c("test@example.com", "test.2@example.edu", "3test@example.net")
 #' )
 #'
-#' redcap_data_dictionary <- dataframe_to_redcap_dictionary(df, "test")
-#' redcap_data_dictionary <- dataframe_to_redcap_dictionary(df, "test", TRUE, "<output_path>.csv")
+#' redcap_data_dictionary <- dataframe_to_redcap_dictionary(df, "pk_col", "test_form")
+#' redcap_data_dictionary <- dataframe_to_redcap_dictionary(
+#' df, "pk_col", "test_form"
+#' TRUE, "<output_path>.csv"
+#' )
 #' }
-dataframe_to_redcap_dictionary <- function(
-    df,
-    form_name,
-    write_to_csv = FALSE,
-    filename = NULL) {
-
-  get_validation_type <- function(col) {
-    col_type <- class(col)[1]
-    switch(col_type,
-           "numeric" = "number",
-           "integer" = "integer",
-           "Date" = "date_dmy",
-           "POSIXct" = "datetime_dmy",
-           "alpha_only")
+dataframe_to_redcap_dictionary <- function(df,
+                                           record_id_col,
+                                           form_name,
+                                           write_to_csv = FALSE,
+                                           filename = NULL) {
+  contains_emails <- function(col) {
+    email_pattern <- "^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}$"
+    return(any(grepl(email_pattern, col)))
   }
 
-  # create unique identifier for each record
-  df$record_id <- seq_len(nrow(df))
-  # make record_id the first column
-  df <- df[, c("record_id", setdiff(names(df), "record_id"))]
+  get_validation_type <- function(col) {
+    if (contains_emails(col)) {
+      return("email")
+    }
+
+    col_type <- class(col)[1]
+    switch(
+      col_type,
+      "numeric" = "number",
+      "integer" = "number",
+      "Date" = "datetime_ymd",
+      "POSIXct" = "datetime_seconds_ymd",
+      as.character(NA)
+    )
+  }
+
+  if (!record_id_col %in% names(df)) {
+    stop("The provided record_id_col does not exist in the input dataframe.")
+  }
+
+  # create the record_id and make it the first column in df
+  df_with_record_id <- df |>
+    dplyr::rename(record_id = tidyselect::all_of(record_id_col)) |>
+    dplyr::select("record_id", dplyr::everything())
+
+  df_with_ordered_cols <- df |>
+    dplyr::select(tidyselect::all_of(record_id_col), dplyr::everything())
 
   redcap_data_dictionary <- data.frame(
-    field_name = names(df),
+    field_name = names(df_with_record_id),
     form_name = form_name,
     section_header = as.character(NA),
     field_type = "text",
-    field_label = names(df),
+    field_label = names(df_with_ordered_cols),
     select_choices_or_calculations = as.character(NA),
     field_note = as.character(NA),
-    text_validation_type_or_show_slider_number = sapply(df, get_validation_type)
+    text_validation_type_or_show_slider_number = sapply(df_with_record_id, get_validation_type)
   )
 
   rownames(redcap_data_dictionary) <- NULL
 
   if (write_to_csv) {
-    if(is.null(filename)) {
+    if (is.null(filename)) {
       stop("Please provide a filename if you want to write to CSV.")
     }
-    utils::write.csv(redcap_data_dictionary, filename, na = "", row.names = FALSE)
+    utils::write.csv(redcap_data_dictionary,
+                     filename,
+                     na = "",
+                     row.names = FALSE)
   }
 
   return(redcap_data_dictionary)
--- R/mirror_data_to_redcap_project.R
+++ R/mirror_data_to_redcap_project.R
@@ -1,24 +1,36 @@
-#' mirror_data_to_redcap_project
+#' Mirror a dataframe to a REDCap project
 #'
-#' mirror a dataframe to a REDCap project
+#' @param df_to_mirror Dataframe to be imported into the REDCap project.
+#' @param record_id_col Column in the dataframe that uniquely identifies each record.
+#' @param project_name REDCap project name.
+#' @param path_to_env_file Path to the ".env" credentials file.
+#' @param ... Additional parameters passed to \code{\link[REDCapR]{redcap_write}}.
 #'
-#' @param df_to_mirror the dataframe to be imported into the REDCap project
-#' @param table_name  the table name
-#' @param path_to_env_file the env file path
-#' @param ... Additional parameters that will be passed to \code{\link[REDCapR]{redcap_write}}
-#' @parm env_file the path to the env file that contains the required credentials
-#'
-#' @return  Same values returned as \code{\link[REDCapR]{redcap_write}}
+#' @return Same values as returned by \code{\link[REDCapR]{redcap_write}}.
 #' @export
+#'
+#' @seealso \code{\link{dataframe_to_redcap_dictionary}} to understand the REDCap data dictionary requirements.
+#' The dictionary must be created and uploaded to REDCap for \code{mirror_data_to_redcap_project} to function properly.
 #'
 #' @examples
 #' \dontrun{
-#' mirror_data <- mirror_data_to_redcap_project(df, "table_name", "./folder/path/.env")
+#' df <- data.frame(
+#'   pk_col = c("a1", "a2", "a3"),
+#'   integer_col = c(1, 2, 3),
+#'   numeric_col = 5.9,
+#'   character_col = c("a", "b", "c"),
+#'   date_col = as.Date("2011-03-07"),
+#'   date_time_col = as.POSIXct(strptime("2011-03-27 01:30:00", "%Y-%m-%d %H:%M:%S")),
+#'   email_col = c("test@example.com", "test.2@example.edu", "3test@example.net")
+#' )
+#' mirrored_data <- mirror_data_to_redcap_project(df, "pk_col", "test_project", "<env_path>.env")
 #' }
 #'
-mirror_data_to_redcap_project <- function(df_to_mirror, table_name, path_to_env_file, ...) {
-
-
+mirror_data_to_redcap_project <- function(df_to_mirror,
+                                          record_id_col,
+                                          project_name,
+                                          path_to_env_file,
+                                          ...) {
   if (!file.exists(path_to_env_file)) {
     stop(sprintf("The .env file was not found at %s", path_to_env_file))
   } else {
@@ -26,7 +38,7 @@ mirror_data_to_redcap_project <- function(df_to_mirror, table_name, path_to_env_
   }
 
   # retrieve the PID value from environment the env file
-  project_id <- paste0(toupper(table_name), "_PID") |>
+  project_id <- paste0(toupper(project_name), "_PID") |>
     Sys.getenv()
 
   credentials_db_path <- Sys.getenv("CREDENTIALS_DB")
@@ -35,7 +47,7 @@ mirror_data_to_redcap_project <- function(df_to_mirror, table_name, path_to_env_
   if (project_id == "") {
     stop(sprintf(
       "Environment variable %s not found in %s.",
-      paste0(toupper(table_name), "_PID"),
+      paste0(toupper(project_name), "_PID"),
       path_to_env_file
     ))
   } else if (credentials_db_path == "") {
@@ -57,17 +69,21 @@ mirror_data_to_redcap_project <- function(df_to_mirror, table_name, path_to_env_
     stop(sprintf("No REDCap API credentials found for PID %s.", project_id))
   }
 
-  # create unique identifier for each record
-  df_to_mirror$record_id <- seq_len(nrow(df_to_mirror))
-  # make record_id the first column
-  df_to_mirror <- df_to_mirror[, c("record_id", setdiff(names(df_to_mirror), "record_id"))]
+  if (!record_id_col %in% names(df_to_mirror)) {
+    stop("The provided record_id_col does not exist in the input dataframe.")
+  }
 
-  rownames(df_to_mirror) <- NULL
+  # create the record_id and make it the first column in df
+  df_to_mirror_with_record_id <- df_to_mirror |>
+    dplyr::rename(record_id = tidyselect::all_of(record_id_col)) |>
+    dplyr::select("record_id", dplyr::everything())
+
+  rownames(df_to_mirror_with_record_id) <- NULL
 
   write_result <- REDCapR::redcap_write(
     redcap_uri = redcap_credentials$redcap_uri,
     token = redcap_credentials$token,
-    ds_to_write = df_to_mirror,
+    ds_to_write = df_to_mirror_with_record_id,
     ...
   )
 
