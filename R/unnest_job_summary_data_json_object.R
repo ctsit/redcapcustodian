60771db4c9c60bf757c27fe2304b236857db0fb5
Philip Chase <pbc@ufl.edu>
2023-10-23 12:39:08 -0400

    Update docs and tests for unnest_job_summary_data_json_object
    Add docs and tests for the objects_to_include parameter.

--- R/unnest_job_summary_data_json_object.R
+++ R/unnest_job_summary_data_json_object.R
@@ -7,13 +7,19 @@
 #' frames into a JSON object, so this has become the common practice.
 #'
 #' Yet using the data can be a bit more complex because `jsonlite::fromJSON` is
-#' not vectorized. If you need read multiple job log records, iteration is
+#' not vectorized. If you need to read multiple job log records, iteration is
 #' required to use JSON object in `job_summary_data`.
 #'
 #' This function addresses that need accepting a dataframe of job summary logs
-#' and unnesting the JSON object(s) in `job_summary_data` into the equivalent R objects.
+#' and unnesting the JSON object(s) in `job_summary_data` into the equivalent
+#' R objects.
+#'
+#' Use `objects_to_include` to limit the unnested objects to those with similar
+#' geometry. Dissimilar objects cannot be unnested together.
 #'
 #' @param log_data - a dataframe of job summary logs
+#' @param objects_to_include - an optional vector of JSON objects in the
+#'   job_summary_data to be un-nested
 #'
 #' @return - the input dataframe of job summary logs with `job_summary_data`
 #'   replaced by the R objects that were JSON-encoded therein.
@@ -32,10 +38,26 @@
 #' log_conn <- get_package_scope_var("log_con")
 #'
 #' log_data <- tbl(log_conn, "rcc_job_log") %>%
-#'   dplyr::filter(script_name == "warn_owners_of_impending_bill") %>%
+#'   dplyr::filter(script_name == "warn_owners_of_impending_bill") |>
 #'   collect()
 #'
 #' unnest_job_summary_data_json_object(log_data)
+#'
+#' # Read a more complex object
+#' log_data_2 <- tbl(log_conn, "rcc_job_log") |>
+#'   dplyr::filter(
+#'     script_name == "create_and_send_new_redcap_prod_per_project_line_items" &
+#'       level == "SUCCESS"
+#'   ) %>%
+#'   collect()
+#'
+#' # Fails due to dissimilar objects
+#' unnest_job_summary_data_json_object(log_data_2)
+#' # error is "Can't recycle `service_instance` (size 27) to match `invoice_line_item` (size 79)."
+#'
+#' # When separated, the objects can be unnested
+#' unnest_job_summary_data_json_object(log_data_2, objects_to_include = c("service_instance"))
+#' unnest_job_summary_data_json_object(log_data_2, objects_to_include = c("invoice_line_item"))
 #' }
 unnest_job_summary_data_json_object <- function(log_data, objects_to_include = NA) {
   log_ids <- log_data$id
--- tests/testthat/test-unnest_job_summary_data_json_object.R
+++ tests/testthat/test-unnest_job_summary_data_json_object.R
@@ -2,21 +2,21 @@ testthat::test_that("unnest_job_summary_data_json_object works", {
   test_data_dir <- "unnest_job_summary_data_json_object"
   log_data <- readRDS(testthat::test_path(test_data_dir, "log_data.rds"))
 
-  sample_size <- 3
-  ncols_in_unnested_data <- 3
+  ncols_in_unnested_data <- 5
 
-  output <- unnest_job_summary_data_json_object(log_data)
+  output <- unnest_job_summary_data_json_object(log_data, objects_to_include = "iris")
 
   testthat::expect_equal(
     output %>%
       dplyr::distinct(id) %>%
       nrow(),
-    sample_size
+    1
     )
 
   testthat::expect_equal(
     output %>%
       dplyr::select(-dplyr::any_of(names(log_data))) %>%
+      pull(iris) %>%
       ncol(),
     ncols_in_unnested_data
   )
