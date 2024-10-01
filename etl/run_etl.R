ac9ebf1b3fddc9f6289eb70d9fc4bd196ca54699
Laurence James-Woodley <mr.lawjames@gmail.com>
2024-10-01 19:10:08 -0400

    allow for optional args

--- etl/run_etl.R
+++ etl/run_etl.R
@@ -6,23 +6,35 @@ library(argparse)
 set_script_run_time()
 
 parser <- ArgumentParser()
-parser$add_argument("script_name", nargs=1, help="Script to be run")
+parser$add_argument("script_name", help="Script to be run")
+parser$add_argument("--optional_args", nargs='*', help="Zero or more optional arguments of any type")
 
 if (!interactive()) {
   args <- parser$parse_args()
-  script_name <- args$script_name
-  if(!fs::file_exists(script_name)) {
-    stop(sprintf("Specified file, %s, does not exist", script_name))
-  }
 } else {
-  script_name <- "etl/dummy_script.R"
-  if(!fs::file_exists(script_name)) {
-    stop(sprintf("Specified file, %s, does not exist", script_name))
-  }
+  args <- parser$parse_args(
+    c(
+      "study_template/etl/test_failure_alert.R",
+      "--optional_args",
+      "test",
+      "another test"
+    )
+  )
+}
+
+script_name <- args$script_name
+optional_args <- args$optional_args
+
+if(!fs::file_exists(script_name)) {
+  stop(sprintf("Specified file, %s, does not exist", script_name))
 }
 
 tryCatch({
-  rscript(script_name, stderr = "log.txt")
+  if (length(optional_args) == 0) {
+    rscript(script = script_name, stderr = "log.txt")
+  } else {
+    rscript(script = script_name, cmdargs = optional_args, stderr = "log.txt")
+  }
 }, error = function(e) {
   email_body <- "See the attached log for error details."
   script_path <- paste(basename(getwd()), script_name, sep = "/")
