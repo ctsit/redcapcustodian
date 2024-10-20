6539710d8e76964169f0f8c2ad73236de073e49b
Laurence James-Woodley <mr.lawjames@gmail.com>
2024-10-20 13:22:55 -0400

    add first draft of render_report

---
+++ R/reporting.R
@@ -0,0 +1,72 @@
+#' Render a Markdown Report from Rmd or Qmd Files
+#'
+#' This function renders a report from a R Markdown (.Rmd) or Quarto Markdown (.Qmd) script.
+#' It is designed to be invoked from `render_report.R`. The rendered report is saved in either PDF or HTML format with a
+#' timestamp appended to the filename.
+#'
+#' @param script_path The full path of the script to render
+#
+#' @return A list containing details about the operation's outcome:
+#'
+#'  If rendering is successful, the list includes:
+#'    \itemize{
+#'      \item `success`: TRUE
+#'      \item `report_name`: The name of the report file
+#'      \item `filepath`: The full path to where the report is saved
+#'  }
+#'   If rendering fails, the list includes:
+#'   \itemize{
+#'     \item `success`: FALSE
+#'      \item `error`: A string describing the error message
+#'  }
+#'
+#' @export
+
+#' @examples
+#' \dontrun{
+#' script_path <- here::here("report", "quarto_html_example.qmd")
+#' report_metadata <- render_report(script_path)
+#' }
+#'
+#'
+render_report <- function(script_path) {
+
+  script_path_without_extension <- tools::file_path_sans_ext(script_path)
+  base_script_name <- basename(script_path_without_extension)
+
+  result <- tryCatch({
+    quarto::quarto_render(script_path)
+    # Return a success indicator if no error occurs
+    list(success = TRUE)
+  }, error = function(e) {
+    # Return failure and error message
+    list(success = FALSE, error = e$message)
+  })
+
+  if (!result$success) {
+    return(result)
+    stop("Report rendering failed: ", result$error)
+  }
+
+  default_filenames <- paste0(script_path_without_extension, c(".pdf", ".html"))
+
+  for (default_filename in default_filenames) {
+    if (file.exists(default_filename)) {
+      file_extension <- tools::file_ext(default_filename)
+
+      report_name <- paste0(
+        base_script_name,
+        "_",
+        format(redcapcustodian::get_script_run_time(), "%Y-%m-%d_%H%M%S"),
+        ".",
+        file_extension
+      )
+
+      report_filepath <- here::here("report", "output", report_name)
+
+      file.rename(default_filename, report_filepath)
+
+      return(list(success = result$success, report_name = report_name, filepath = report_filepath))
+    }
+  }
+}
--- report/render_report.R
+++ report/render_report.R
@@ -1,8 +1,5 @@
 library(tidyverse)
 library(dotenv)
-library(REDCapR)
-library(lubridate)
-library(rmarkdown)
 library(sendmailR)
 library(redcapcustodian)
 library(argparse)
@@ -10,58 +7,33 @@ library(argparse)
 init_etl("render_report")
 
 parser <- ArgumentParser()
-parser$add_argument("script_name", nargs=1, help="Script to be run")
+parser$add_argument("script_path", nargs=1, help="The full path of the script to be run")
 if (!interactive()) {
   args <- parser$parse_args()
-  script_name <- args$script_name
-  if(!fs::file_exists(script_name)) {
-    stop(sprintf("Specified file, %s, does not exist", script_name))
+  script_path <- args$script_path
+  if(!fs::file_exists(script_path)) {
+    stop(sprintf("Specified file, %s, does not exist", script_path))
   }
 } else {
-  script_name <- "report/sample_report.qmd"
-  if(!fs::file_exists(script_name)) {
-    stop(sprintf("Specified file, %s, does not exist", script_name))
+  script_path <- "report/sample_report.Rmd"
+  if(!fs::file_exists(script_path)) {
+    stop(sprintf("Specified file, %s, does not exist", script_path))
   }
 }
 
-report_name <- word(script_name, 1, sep = "\\.")
-report_type <- word(script_name, 2, sep = "\\.")
+render_results <- render_report(script_path)
 
-script_run_time <- set_script_run_time()
-output_filename <-
-  paste0(
-    str_replace(report_name, ".*/", ""),
-    "_",
-    format(script_run_time, "%Y%m%d%H%M%S")
-  )
+if (render_results$success) {
+  email_subject <- paste(render_results$report_name)
+  attachment_object <- mime_part(render_results$filepath)
+  body <- "Please see the attached report."
+  email_body <- list(body, attachment_object)
 
-if (report_type == "qmd") {
-  full_path_to_output_file <- quarto::quarto_render(
-    script_name,
-    output_file = paste0(output_filename, ".pdf"),
-    output_format = "pdf"
-  )
+  send_email(email_body, email_subject)
 
-  attachment_object <- mime_part(paste0(output_filename, ".pdf"))
+  # log_job_success(jsonlite::toJSON(basename(script_path)))
 } else {
-  full_path_to_output_file <- render(
-    script_name,
-    output_file = output_filename
-  )
-
-  attachment_object <- mime_part(
-    full_path_to_output_file,
-    basename(full_path_to_output_file)
-  )
+  email_body <- "Report failed to render."
+  email_subject <- paste0("Failed | ", here::here(script_path), " | ", format(get_script_run_time(), "%Y-%m-%d"))
+  send_email(email_body, email_subject)
 }
-
-email_subject <- paste(basename(report_name), "|", script_run_time)
-body <- "Please see the attached report."
-
-email_body <- list(body, attachment_object)
-
-# send the email with the attached output file
-send_email(email_body, email_subject)
-
-set_script_name("render_report")
-log_job_success(jsonlite::toJSON(script_name))
