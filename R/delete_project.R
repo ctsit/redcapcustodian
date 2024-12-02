2a71c4be2e7b5bf0ff53b85d12be1d4406b71287
Sai Pavan Kamma <saipavankamma@ufl.edu>
2024-12-02 20:39:26 +0530

    Improve logging for delete_project function

--- R/delete_project.R
+++ R/delete_project.R
@@ -19,12 +19,11 @@
 #' @examples
 #' \dontrun{
 #' conn <- DBI::dbConnect(...)
-#' delete_project(c(1,2,3), conn)
+#' delete_project(c(1, 2, 3), conn)
 #' }
 #' @export
 
 delete_project <- function(project_id, conn) {
-
   redcap_projects <- DBI::dbGetQuery(
     conn,
     sprintf(
@@ -34,47 +33,86 @@ delete_project <- function(project_id, conn) {
         log_event_table
      from redcap_projects
      where project_id in (%s)",
-     paste0(project_id, collapse = ",")
+      paste0(project_id, collapse = ",")
     )
   )
 
-  # select projects for deletion
   projects_to_delete <- redcap_projects[is.na(redcap_projects$date_deleted), ]
   redcap_project_ids <- projects_to_delete$project_id
   redcap_log_tables <- projects_to_delete$log_event_table
 
-  if (nrow(projects_to_delete) > 0) {
-    tryCatch({
-      deleted_projects <- DBI::dbExecute(
-        conn,
-        sprintf(
-          "update redcap_projects set date_deleted = now() where project_id in (%s)",
-          paste0(redcap_project_ids, collapse = ",")
-        )
-      )
-    }, error = function(error_message) {
-      print(error_message)
-      return(FALSE)
-    })
 
-    # log the event
-    tryCatch({
-      inserted_rows <- purrr::map2(
-        redcap_log_tables,
-        redcap_project_ids,
-        ~ DBI::dbExecute(
-          conn,
-          sprintf(
-            "insert into %s (object_type, event, project_id, description)
-             values ('redcap_projects', 'MANAGE', %d, 'delete project')",
-        .x,
-        .y)
+  if (nrow(projects_to_delete) > 0) {
+    delete_sql <- sprintf(
+      "UPDATE redcap_projects SET date_deleted = NOW() WHERE project_id IN (%s)",
+      paste0(redcap_project_ids, collapse = ",")
+    )
+
+    tryCatch(
+      {
+        deleted_projects <- DBI::dbExecute(conn, delete_sql)
+      },
+      error = function(error_message) {
+        print(error_message)
+        return(FALSE)
+      }
+    )
+
+    # Define logging parameters
+    ts <- format(Sys.time(), "%Y%m%d%H%M%S") # Time stamp
+    user <- "admin" # Placeholder for user
+    ip <- "192.168.65.1" # Placeholder for IP address
+    page <- "rcc.billing::delete_abandoned_projects"
+    event <- "MANAGE"
+    object_type <- "redcap_projects"
+    description <- "Delete project"
+    legacy <- 0
+    change_reason <- NULL
+
+    tryCatch(
+      {
+        inserted_rows <- purrr::map2(
+          redcap_log_tables,
+          redcap_project_ids,
+          ~ {
+            pk <- .y
+            data_values <- sprintf("project_id = %d", .y)
+
+            DBI::dbExecute(
+              conn,
+              sprintf(
+                "INSERT INTO %s
+                (log_event_id, project_id, ts, user, ip, page, event,
+                 object_type, sql_log, pk, event_id, data_values,
+                 description, legacy, change_reason)
+               VALUES
+                (NULL, %d, '%s', '%s', '%s', '%s', '%s',
+                 '%s', '%s', '%d', NULL, '%s',
+                 '%s', %d, %s)",
+                .x, # Log table
+                .y, # Project ID
+                ts,
+                user,
+                ip,
+                page,
+                event,
+                object_type,
+                delete_sql,
+                pk,
+                data_values,
+                description,
+                legacy,
+                ifelse(is.null(change_reason), "NULL", sprintf("'%s'", change_reason)) # Change reason
+              )
+            )
+          }
         )
-      )
-    }, error = function(error_message) {
-      print(error_message)
-      return(FALSE)
-    })
+      },
+      error = function(error_message) {
+        print(error_message)
+        return(FALSE)
+      }
+    )
   } else {
     deleted_projects <- NULL
     inserted_rows <- NULL
